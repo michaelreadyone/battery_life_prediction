eda7f526eb6409549a3bd739972ae1db369ee49f
mike <michael56653556@gmail.com>
2025-06-21 18:08:28 -0400

    add

---
+++ rul_model.py
@@ -0,0 +1,121 @@
+import numpy as np
+import math
+import torch
+import torch.nn as nn
+from utils import get_train_test, get_train_test_from_df, setup_seed
+import pandas as pd
+
+seed=0
+setup_seed(seed)
+
+K = 16
+Rated_Capacity = 1.1
+feature_size = 64
+feature_num = K
+dropout = 0.0
+epochs = 500
+nhead = 1
+hidden_dim = 16
+num_layers = 3
+lr = 0.001  
+weight_decay = 0.0
+noise_level = 0.0
+alpha = 0.1
+metric = 're'
+seed = 0
+device = "cpu"
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, feature_len, feature_size, dropout=0.0):
+        # feature_len: max length of sequence, feature_size: embedding size(d_model)
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(feature_len, feature_size)
+        position = torch.arange(0, feature_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, feature_size, 2).float() * (-math.log(10000.0) / feature_size))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        # print(f'pe.shape: {pe.shape}')
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe
+
+  
+class Transformer(nn.Module):
+    def __init__(self, feature_size, hidden_dim, feature_num, num_layers, nhead):
+        super(Transformer, self).__init__()
+        half_feature_size = int(feature_size)
+        print(f'dropout: {dropout}')
+        
+        self.autoencoder = nn.Linear(feature_size, half_feature_size)
+        self.pe = PositionalEncoding(half_feature_size, feature_num)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=feature_num, nhead=nhead, dim_feedforward=hidden_dim, dropout=dropout, batch_first=True)
+        self.layers = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        self.lm_head = nn.Linear(feature_num*half_feature_size, 1)
+        
+    def forward(self, x):
+        B,T,C = x.shape
+        x = self.autoencoder(x)
+        x = x.reshape(B, -1, T)
+        x = self.pe(x)
+        x = self.layers(x)
+        x = x.reshape(B, -1)
+        x = self.lm_head(x)
+        return x
+    
+def load_data_from_npy():
+    Battery = np.load('datasets/CALCE/CALCE.npy', allow_pickle=True)
+    Battery = Battery.item() # A dict with cell name as key and dataframe as value
+    name = 'CS2_35'
+    feature_size = 64
+    train_x, train_y, train_data, test_data = get_train_test(Battery, name, feature_size)
+    x = np.reshape(train_x/Rated_Capacity,(-1, 1, feature_size)).astype(np.float32)
+    y = np.reshape(train_y/Rated_Capacity,(-1,1)).astype(np.float32) 
+
+    x, y = torch.from_numpy(x).to(device), torch.from_numpy(y).to(device)
+    x = x.repeat(1, K, 1)
+    return x, y
+
+def load_data_from_csv():
+    battery_df = pd.read_csv('datasets/CALCE/CALCE.csv')
+    name = 'CS2_35'
+    feature_size = 64
+    train_x, train_y, train_data, test_data = get_train_test_from_df(battery_df, name, feature_size)
+    x = np.reshape(train_x/Rated_Capacity,(-1, 1, feature_size)).astype(np.float32)
+    y = np.reshape(train_y/Rated_Capacity,(-1,1)).astype(np.float32) 
+
+    x, y = torch.from_numpy(x).to(device), torch.from_numpy(y).to(device)
+    x = x.repeat(1, K, 1)
+    return x, y
+    
+    
+if __name__ == "__main__":
+    
+    x, y = load_data_from_npy()
+    # x, y = load_data_from_csv()
+    model = Transformer(feature_size, hidden_dim, feature_num, num_layers, nhead)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    losses = []
+    
+    for epoch in range(epochs):
+        output = model(x)
+        output = output.reshape(-1, 1)
+        loss = nn.MSELoss()(output, y)
+        losses.append(loss.item())
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (epoch+1) % 10 == 0:
+            loss_avg = np.mean(losses[-10:])
+            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, epochs, loss_avg))
+        
+        
+    
+    
+    
+    
--- utils.py
+++ utils.py
@@ -8,10 +8,13 @@ import torch
 
 def build_sequences(text, window_size):
     #text:list of capacity
+    # print(f'text: {text}')
     x, y = [],[]
     for i in range(len(text) - window_size):
         sequence = text[i:i+window_size]
+        # print(f'sequence: {sequence}')
         target = text[i+window_size]
+        # print(f'target: {target}')
 
         x.append(sequence)
         y.append(target)
@@ -23,11 +26,37 @@ def get_train_test(data_dict, name, window_size=8):
     data_sequence=data_dict[name]['capacity']
     train_data, test_data = data_sequence[:window_size+1], data_sequence[window_size+1:]
     # why add test data?
-    train_x, train_y = build_sequences(text=train_data, window_size=window_size)
+    # train_x, train_y = build_sequences(text=train_data, window_size=window_size)
+    train_x, train_y = np.empty((0, window_size)), np.empty((0,))
+
     for k, v in data_dict.items():
         if k != name:
             data_x, data_y = build_sequences(text=v['capacity'], window_size=window_size)
             train_x, train_y = np.r_[train_x, data_x], np.r_[train_y, data_y]
+            
+    return train_x, train_y, list(train_data), list(test_data)
+
+def get_train_test_from_df(battery_df, name, window_size):
+    battery_names = list(battery_df['cell_name'].unique())
+    print(f'battery_names: {battery_names}')
+    
+    test_df = battery_df[battery_df['cell_name'] == name]
+    data_sequence=test_df['capacity']
+    train_data, test_data = data_sequence[:window_size+1], data_sequence[window_size+1:]
+    # why add test data?
+    # train_x, train_y = build_sequences(text=train_data, window_size=window_size)
+    train_x, train_y = np.empty((0, window_size)), np.empty((0,))
+    
+    
+
+    for battery_name in battery_names:
+        if battery_name == name:
+            continue
+        cell_df = battery_df[battery_df['cell_name'] == battery_name].reset_index(drop=True)
+        # print(f'cell_df: {cell_df}')
+
+        data_x, data_y = build_sequences(text=cell_df['capacity'], window_size=window_size)
+        train_x, train_y = np.r_[train_x, data_x], np.r_[train_y, data_y]
             
     return train_x, train_y, list(train_data), list(test_data)
 
